2e9f3899c60dc1e22c661b56b80b01fcd7070483
ROMAN SHESTAKOV <romanshestakov@iMac.local>
2017-07-08 14:32:28 +0100

    add missed files for centos

---
+++ provision/scripts/centos7/ansible.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eux
+
+# Install EPEL repository.
+rpm -ivh http://mirror.pnl.gov/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+
+# Install Ansible.
+yum -y install git python-setuptools gcc sudo libffi-devel python-devel openssl-devel wget
+yum clean all
+easy_install pip
+pip install ansible
---
+++ provision/scripts/centos7/vagrant_key.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#yum install wget -y
+#apt-get -y update && apt-get -y upgrade
+# sudo yum -y install wget
+
+mkdir /home/vagrant/.ssh
+wget --no-check-certificate \
+    'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' \
+    -O /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+chmod -R go-rwsx /home/vagrant/.ssh
